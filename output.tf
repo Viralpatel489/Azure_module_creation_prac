2444851839b9e9412f1730984cc5d26dd1cd39a9
Viralpatel489 <viralpatel489@gmail.com>
2025-10-20 13:25:52 -0400

    initial commit of NIC module

---
+++ output.tf
@@ -0,0 +1,11 @@
+output "nic_name" {
+    value = azurerm_network_interface.nic.name
+    description = "nic name"
+  
+}
+
+output "nic_id" {
+    value = azurerm_network_interface.nic.id
+    description = "nic id"
+
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "nic_name" {}
+variable "rg_name" {}
+variable "location" {}
+variable "subnet" {}
+variable "subnet_id" {}
+
+
