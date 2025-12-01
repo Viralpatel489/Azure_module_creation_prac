9f66b2d3662dd834f2d2d631c13af9d96da16c48
Viral Patel <viralpatel489@gmail.com>
2025-12-01 13:20:37 -0500

    Add output for subnet ID in output.tf

--- subnet/output.tf
+++ subnet/output.tf
@@ -7,3 +7,8 @@ output "subnet_prefix" {
     description = "subnet prefix"
     value = azurerm_subnet.subnet.address_prefixes
 }
+# Inside Azure_module_creation_prac/subnet/outputs.tf
+output "id" {
+  description = "The ID of the Subnet."
+  value       = azurerm_subnet.subnet.id
+}
