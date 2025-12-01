53b8281c6c1edad7a74d0470dc41ce7a74625ca7
Viral Patel <viralpatel489@gmail.com>
2025-12-01 12:38:54 -0500

    Rename rg_name to resource_group_name in main.tf

--- nic/main.tf
+++ nic/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_network_interface" "nic" {
     name = var.nic_name
     location = var.location
-    rg_name = var.rg_name
+    resource_group_name = var.rg_name
     ip_configuration {
       name = "internal"
       subnet_id = var.subnet_id
