228d7f02d5d70605b3fb4e090995b22b8f8a77c5
Viral Patel <viralpatel489@gmail.com>
2025-12-02 12:22:33 -0500

    Add public IP address configuration to NIC

--- nic/main.tf
+++ nic/main.tf
@@ -6,5 +6,6 @@ resource "azurerm_network_interface" "nic" {
       name = "internal"
       subnet_id = var.subnet_id
       private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.pip.id
     }
 }
