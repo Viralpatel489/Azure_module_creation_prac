15a8ee5dcc83323ea612f9b8e653219e985dae10
Viral Patel <viralpatel489@gmail.com>
2025-12-11 13:39:40 -0500

    Add vnet_id output to output.tf

--- Vnet/output.tf
+++ Vnet/output.tf
@@ -8,3 +8,7 @@ output "address_space" {
   description = "vnet adress space"
   value       = azurerm_virtual_network.vnet.address_space
 }
+
+output "vnet_id" {
+  value = azurerm_virtual_network.vnet.id
+}
