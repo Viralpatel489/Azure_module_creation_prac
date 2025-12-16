4bd9b58f45d45f2ec81008d6fca1bf2bb2dec6b0
Viral Patel <viralpatel489@gmail.com>
2025-12-16 12:25:51 -0500

    Refactor NSG variables to use security_rules list
    Replaced individual security rule variables with a list of security rules as an object.

--- nsg/variables.tf
+++ nsg/variables.tf
@@ -1,12 +1,26 @@
 variable "nsg_name" {}
 variable "location" {}
 variable "rg_name" {}
-variable "security_rule_name" {}
-variable "priority" {}
-variable "direction" {}
-variable "access" {}
-variable "protocol" {}
-variable "source_port_range" {}
-variable "destination_port_range" {}
-variable "source_address_prefix" {}
-variable "destination_address_prefix" {}
+variable "security_rules" {
+  description = "List of security rules for NSG"
+  type = list(object({
+    name                       = string
+    priority                   = number
+    direction                  = string
+    access                     = string
+    protocol                   = string
+    source_port_range          = string
+    destination_port_range     = string
+    source_address_prefix      = string
+    destination_address_prefix = string
+  }))
+}
+# variable "security_rule_name" {}
+# variable "priority" {}
+# variable "direction" {}
+# variable "access" {}
+# variable "protocol" {}
+# variable "source_port_range" {}
+# variable "destination_port_range" {}
+# variable "source_address_prefix" {}
+# variable "destination_address_prefix" {}
