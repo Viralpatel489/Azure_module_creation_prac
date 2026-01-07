0854959c31142f28164e00def4bf291534c5ee69
Viralpatel489 <viralpatel489@gmail.com>
2026-01-07 10:46:51 -0500

    change resource group variable to rg_name

--- dynamic_nsg/main.tf
+++ dynamic_nsg/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_network_security_group" "nsg" {
   name                = var.nsg_name
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = var.rg_name
 
 # The "security_rule" block is the one that repeats inside the NSG.
   dynamic "security_rule" {
--- dynamic_nsg/variables.tf
+++ dynamic_nsg/variables.tf
@@ -1,4 +1,4 @@
-variable "resource_group_name" {
+variable "rg_name" {
     description = "The name of the resource group"
     type        = string
 }
