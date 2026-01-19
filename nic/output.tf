d0e6e7acd0c823500ab43cd36e919b20b6ee7dbe
Viralpatel489 <viralpatel489@gmail.com>
2026-01-19 12:34:40 -0500

    change nic_private_ip_address

--- nic/output.tf
+++ nic/output.tf
@@ -8,7 +8,7 @@ output "nic_id" {
   description = "nic id"
 }
 
-output "nic_private_ip_address" {
+output "private_ip_address" {
   value = azurerm_network_interface.nic.private_ip_address
   description = "nic private ip address"
 }
